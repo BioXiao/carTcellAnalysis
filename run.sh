a4ba8658b00e1cc89c5daa55363107fe60cf6ad4
James Eddy <james.a.eddy@gmail.com>
2015-12-23 15:04:36 -0800

    doing some additional testing

--- run.sh
+++ run.sh
@@ -6,31 +6,42 @@
 # python scripts/carGeneSalmon.py $INPUT_DIR $OUTPUT_BASE
 
 
-INPUT_DIR="/Volumes/genomics/Illumina/150910_D00565_0096_AC6REVANXX/Project_P89-5Processed_150916/TrimmedFastqs/"
-OUTPUT_BASE="data/results/P89-5_150910"
+# INPUT_DIR="/Volumes/genomics/Illumina/150910_D00565_0096_AC6REVANXX/Project_P89-5Processed_150916/TrimmedFastqs/"
+# OUTPUT_BASE="data/results/P89-5_150910"
+#
+# python scripts/carGeneSalmon.py $INPUT_DIR $OUTPUT_BASE
+#
+#
+# INPUT_DIR="/Volumes/genomics/Illumina/150910_D00565_0096_AC6REVANXX/Project_P89-7Processed_150916/TrimmedFastqs/"
+# OUTPUT_BASE="data/results/P89-7_150910"
+#
+# python scripts/carGeneSalmon.py $INPUT_DIR $OUTPUT_BASE
+#
+#
+# INPUT_DIR="/Volumes/genomics/Illumina/150910_D00565_0096_AC6REVANXX/Project_P89-9Processed_150916/TrimmedFastqs/"
+# OUTPUT_BASE="data/results/P89-9_150910"
+#
+# python scripts/carGeneSalmon.py $INPUT_DIR $OUTPUT_BASE
+#
+#
+# INPUT_DIR="/Volumes/genomics/Illumina/150729_D00565_0092_AC6VC6ANXX/Project_P89-7Processed_150916/TrimmedFastqs/"
+# OUTPUT_BASE="data/results/P89-7_150729"
+#
+# python scripts/carGeneSalmon.py $INPUT_DIR $OUTPUT_BASE
+#
+#
+# INPUT_DIR="/Volumes/genomics/Illumina/150729_D00565_0092_AC6VC6ANXX/Project_P89-9Processed_150916/TrimmedFastqs/"
+# OUTPUT_BASE="data/results/P89-9_150729"
+#
+# python scripts/carGeneSalmon.py $INPUT_DIR $OUTPUT_BASE
+
+
+INPUT_DIR="/Volumes/genomics/Illumina/150528_D00565_0086_BC6VC8ANXX/Project_P85-5Processed_new/TrimmedFastqs/"
+OUTPUT_BASE="data/results/P85-5_150528"
 
 python scripts/carGeneSalmon.py $INPUT_DIR $OUTPUT_BASE
 
-
-INPUT_DIR="/Volumes/genomics/Illumina/150910_D00565_0096_AC6REVANXX/Project_P89-7Processed_150916/TrimmedFastqs/"
-OUTPUT_BASE="data/results/P89-7_150910"
-
-python scripts/carGeneSalmon.py $INPUT_DIR $OUTPUT_BASE
-
-
-INPUT_DIR="/Volumes/genomics/Illumina/150910_D00565_0096_AC6REVANXX/Project_P89-9Processed_150916/TrimmedFastqs/"
-OUTPUT_BASE="data/results/P89-9_150910"
-
-python scripts/carGeneSalmon.py $INPUT_DIR $OUTPUT_BASE
-
-
-INPUT_DIR="/Volumes/genomics/Illumina/150729_D00565_0092_AC6VC6ANXX/Project_P89-7Processed_150916/TrimmedFastqs/"
-OUTPUT_BASE="data/results/P89-7_150729"
-
-python scripts/carGeneSalmon.py $INPUT_DIR $OUTPUT_BASE
-
-
-INPUT_DIR="/Volumes/genomics/Illumina/150729_D00565_0092_AC6VC6ANXX/Project_P89-9Processed_150916/TrimmedFastqs/"
-OUTPUT_BASE="data/results/P89-9_150729"
+INPUT_DIR="/Volumes/genomics/Illumina/150528_D00565_0086_BC6VC8ANXX/Project_P85-6Processed_new/TrimmedFastqs/"
+OUTPUT_BASE="data/results/P85-6_150528"
 
 python scripts/carGeneSalmon.py $INPUT_DIR $OUTPUT_BASE
