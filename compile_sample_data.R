cb01208011c2deee098d7a16ee6e1b01e4013fbc
James Eddy <james.a.eddy@gmail.com>
2016-03-14 15:54:21 -0700

    code to read/compile sample and metric data

--- compile_sample_data.R
+++ compile_sample_data.R
@@ -5,11 +5,12 @@ library(readr)
 library(readxl)
 library(stringr)
 library(dplyr)
+library(tidyr)
 
 
 # specify file paths ------------------------------------------------------
 
-file_list <- list(
+sample_file_list <- list(
     p89_5_ip = "/Volumes/genomics/Illumina/150910_D00565_0096_AC6REVANXX/For analyst/P89 - Cameron/P89-5/P89-5_LibAnnotation_x194IP_2_1771023248_24Aug2015.xlsx",
     p89_5_d7 = "/Volumes/genomics/Illumina/150910_D00565_0096_AC6REVANXX/For analyst/P89 - Cameron/P89-5/P89-5_C1 LibAnnotaion_x194Day7_1771023246_19Aug2015.xlsx",
     p89_5_d28 = "/Volumes/genomics/Illumina/150910_D00565_0096_AC6REVANXX/For analyst/P89 - Cameron/P89-5/P89-5_C1 LibAnnotation_x194Day28_1771029069_2015Jul27.xlsx",
@@ -24,6 +25,25 @@ file_list <- list(
 p89_bulk <- "/Volumes/genomics/Illumina/150910_D00565_0096_AC6REVANXX/For analyst/P89 - Cameron/Bulk samples P89-6, -8, -10. -11, -12, -13/P89 bulk libs.xlsx"
 
 
+# specify metric file paths -----------------------------------------------
+
+metric_file_list <- list(
+    p89_5 = "/Volumes/genomics/Illumina/150910_D00565_0096_AC6REVANXX/Project_P89-5Processed_150916/metrics/P89-5_C6REVANXX_150916_combined_metrics.csv",
+    p89_7_1 = "/Volumes/genomics/Illumina/150729_D00565_0092_AC6VC6ANXX/Project_P89-7Processed_150916/metrics/P89-7_C6VC6ANXX_150916_combined_metrics.csv",
+    p89_7_2 = "/Volumes/genomics/Illumina/150910_D00565_0096_AC6REVANXX/Project_P89-7Processed_150916/metrics/P89-7_C6REVANXX_150916_combined_metrics.csv",
+    p89_9_1 = "/Volumes/genomics/Illumina/150729_D00565_0092_AC6VC6ANXX/Project_P89-9Processed_150916/metrics/P89-9_C6VC6ANXX_150916_combined_metrics.csv",
+    p89_9_2 = "/Volumes/genomics/Illumina/150910_D00565_0096_AC6REVANXX/Project_P89-9Processed_150916/metrics/P89-9_C6REVANXX_150916_combined_metrics.csv"
+)
+
+bulk_metric_file_list <- list(
+    p89_6 = "/Volumes/genomics/Illumina/150910_D00565_0096_AC6REVANXX/Project_P89-6Processed_150917/metrics/P89-6_C6REVANXX_150917_combined_metrics.csv",
+    p89_8 = "/Volumes/genomics/Illumina/150910_D00565_0096_AC6REVANXX/Project_P89-8Processed_150917/metrics/P89-8_C6REVANXX_150917_combined_metrics.csv",
+    p89_10 = "/Volumes/genomics/Illumina/150910_D00565_0096_AC6REVANXX/Project_P89-10Processed_150917/metrics/P89-10_C6REVANXX_150917_combined_metrics.csv",
+    p89_11 = "/Volumes/genomics/Illumina/150910_D00565_0096_AC6REVANXX/Project_P89-11Processed_150917/metrics/P89-11_C6REVANXX_150917_combined_metrics.csv",
+    p89_12 = "/Volumes/genomics/Illumina/150910_D00565_0096_AC6REVANXX/Project_P89-12Processed_150917/metrics/P89-12_C6REVANXX_150917_combined_metrics.csv",
+    p89_13 = "/Volumes/genomics/Illumina/150910_D00565_0096_AC6REVANXX/Project_P89-13Processed_150917/metrics/P89-13_C6REVANXX_150917_combined_metrics.csv"
+)
+
 # utility functions -------------------------------------------------------
 
 snake_case <- function(str) {
@@ -35,9 +55,9 @@ df_to_snake_case <- function(df) {
     names(df) <- snake_case(names(df))
     return(df)
 }
-# load data ---------------------------------------------------------------
+# load sample data ---------------------------------------------------------------
 
-sc_lib_dat <- lapply(file_list, function(x) {
+sc_lib_dat <- lapply(sample_file_list, function(x) {
     read_excel(x) %>% 
         df_to_snake_case()
 }) %>% 
@@ -49,4 +69,22 @@ bulk_lib_dat <- read_excel(p89_bulk) %>%
     df_to_snake_case() %>% 
     separate(sample_name, c("donor_id", "timepoint"), 
              remove = FALSE, sep = "_", extra = "drop")
+
+
+# load metrics data -------------------------------------------------------
+
+sc_metric_dat <- lapply(metric_file_list, function(x) {
+    read_csv(x) %>% 
+        df_to_snake_case()
+}) %>% 
+    bind_rows() %>% 
+    filter(!is.na(libid))
+
+bulk_metric_dat <- lapply(bulk_metric_file_list, function(x) {
+    read_csv(x) %>% 
+        df_to_snake_case()
+}) %>% 
+    bind_rows() %>% 
+    filter(!is.na(libid))
+
 
