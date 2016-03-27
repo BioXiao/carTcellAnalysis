37ef4643f84239c6a51ef95b497aa596f3a0b9b6
James Eddy <james.a.eddy@gmail.com>
2016-03-26 19:27:48 -0700

    completed and cleaned up new version of pipeline

--- carDetectPipe.sh
+++ carDetectPipe.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SAMPLE_LIST=$1
+
 # Define directory paths
 SEQUENCE_DIR="data/sequence"
 RAPMAP_INDEX_DIR="data/indexes/rapmap"
@@ -12,54 +14,98 @@ SALMON_EXEC="tools/SalmonBeta-0.5.0_OSX-10.10/bin/salmon"
 
 # Build indexes for the modified reference transcriptome, which includes the
 # gene construct sequence as an additional transcript
-HG19_CAR_FASTA="${SEQUENCE_DIR}/hg19_CAR_transcripts.fa"
+HG38_CAR_FASTA="${SEQUENCE_DIR}/hg38_CAR_transcripts.fa"
 
-# Build RapMap quasiindex for hg19 + CAR transcript
-HG19_CAR_RAPMAP_INDEX="${INDEX_DIR}/hg19_CAR"
-if [ ! -e "$HG19_CAR_RAPMAP_INDEX" ]; then
-    echo "Building hg19 + CAR RapMap quasiindex..."
+# Build RapMap quasiindex for hg38 + CAR transcript
+HG38_CAR_RAPMAP_INDEX="${RAPMAP_INDEX_DIR}/hg38_CAR"
+if [ ! -e "$HG38_CAR_RAPMAP_INDEX" ]; then
+    echo "Building hg38 + CAR RapMap quasiindex..."
     $RAPMAP_EXEC quasiindex \
-        -t $HG19_CAR_FASTA \
-        -i $HG19_CAR_RAPMAP_INDEX \
+        -t $HG38_CAR_FASTA \
+        -i $HG38_CAR_RAPMAP_INDEX \
         -k 19
 fi
 
-# Build RapMap quasiindex for hg19 + CAR transcript
-HG19_CAR_SALMON_INDEX="${INDEX_DIR}/hg19_CAR"
-if [ ! -e "$HG19_CAR_SALMON_INDEX" ]; then
-    echo "Building hg19 + CAR Salmon quasiindex..."
+# Build RapMap quasiindex for hg38 + CAR transcript
+HG38_CAR_SALMON_INDEX="${SALMON_INDEX_DIR}/hg38_CAR"
+if [ ! -e "$HG38_CAR_SALMON_INDEX" ]; then
+    echo "Building hg38 + CAR Salmon quasiindex..."
     $SALMON_EXEC index \
-        -t $HG19_CAR_FASTA \
-        -i $HG19_CAR_SALMON_INDEX \
+        -t $HG38_CAR_FASTA \
+        -i $HG38_CAR_SALMON_INDEX \
         --type quasi \
         -k 19
 fi
 
-# Run RapMap and Salmon on all input FASTQs
-SAMPLE_LIST=#
+RAPMAP_OUT_DIR="data/results/rapmap"
+SALMON_OUT_DIR="data/results/salmon"
 
-# Run RapMap on all samples
-while read input; do
-    in_file=${input##*/}
-    lib_id=${in_file%.*}
-    out_prefix="data/results/rapmap/P89_bulk_hg38/${lib_id}_hg38_CAR"
+XCRIPT_LIST=$(grep ">" ${SEQUENCE_DIR}/combined_CAR_endo.fa \
+              | awk -F" " '{gsub(">", ""); print $1}')
 
-    printf "\n>> Quasimap %s to %s...\n" "${in_file}" "${HG38_CAR_INDEX}"
-    # if [ ! -e "${out_prefix}.bam" ]; then
+# Run RapMap and Salmon on all samples
+while read line; do
+    start_time=$(date +%s)
+    sleep 1
+
+    file=$(echo $line | awk '{print $1}')
+    lib_id=$(echo $file | egrep -o -e "lib[0-9]+.*XX")
+    fc=$(echo $line | awk '{print $2}')
+    proj=$(echo $line | awk '{print $3}')
+
+    out_subdir="${fc}/${proj}"
+    if [ ! -e "${RAPMAP_OUT_DIR}/${out_subdir}" ]; then
+        mkdir -p "${RAPMAP_OUT_DIR}/${out_subdir}"
+    fi
+
+    if [ ! -e "${SALMON_OUT_DIR}/${out_subdir}" ]; then
+        mkdir -p "${SALMON_OUT_DIR}/${out_subdir}"
+    fi
+
+    # Run RapMap and format output
+    tmp_dir="data/tmp"
+    if [ ! -e "$tmp_dir" ]; then
+        mkdir "$tmp_dir"
+    fi
+    tmp_prefix="${tmp_dir}/${lib_id}"
+    out_prefix="${RAPMAP_OUT_DIR}/${out_subdir}/${lib_id}"
+    printf "\n>> Quasimap %s to %s...\n" "${lib_id}" "${HG38_CAR_RAPMAP_INDEX}"
+    if [ ! -e "${out_prefix}.bam" ]; then
+        if [ ! -e "${tmp_dir}/$(basename $file)" ]; then
+            time rsync -h --progress $file "${tmp_dir}/$(basename $file)"
+        fi
         time $RAPMAP_EXEC quasimap -t 4 \
-        -i $HG38_CAR_INDEX \
-        -r $fastq \
-        | $SAMTOOLS_EXEC view -bS - \
-        | $SAMTOOLS_EXEC sort - $out_prefix && \
-        $SAMTOOLS_EXEC index ${out_prefix}.bam
-    # fi
-done < <(find ${DATA_DIR} -name "*fastq*")
+            -i $HG38_CAR_RAPMAP_INDEX \
+            -r "${tmp_dir}/$(basename $file)" > ${tmp_prefix}.sam
 
-# Run Salmon on all samples
-while read fastq; do
-    $SALMON_EXEC quant \
-        -i $GENE_XCRIPT_PLUS_INDEX \
-        -l U \
-        -r $SIM_FASTQ_NO_MULTI \
-        -o $GENE_XCRIPT_PLUS_OUT_DIR_1
-done < #
+        echo "Formatting output..."
+        time ($SAMTOOLS_EXEC view -bS -@ 4 ${tmp_prefix}.sam \
+            > ${tmp_prefix}.bam && \
+            $SAMTOOLS_EXEC sort -@ 4 ${tmp_prefix}.bam ${tmp_prefix}_sorted && \
+            $SAMTOOLS_EXEC index -@ 4 ${tmp_prefix}_sorted.bam && \
+            $SAMTOOLS_EXEC view -b -@ 4 ${tmp_prefix}_sorted.bam $XCRIPT_LIST \
+            > ${out_prefix}.bam && \
+            $SAMTOOLS_EXEC index -@ 4 ${out_prefix}.bam)
+    else
+        echo "Output already exists."
+    fi
+    rm -rf $tmp_dir
+
+    # Run Salmon
+    out_prefix="${SALMON_OUT_DIR}/${out_subdir}/${lib_id}"
+    printf "\n>> Quant %s with %s...\n" "${lib_id}" "${HG38_CAR_SALMON_INDEX}"
+    if [ ! -e "${out_prefix}/quant.sf" ]; then
+        time $SALMON_EXEC quant \
+            -i $HG38_CAR_SALMON_INDEX \
+            -l U \
+            -r $file \
+            -o $out_prefix
+    else
+        echo "Output already exists."
+    fi
+
+    end_time=$(date +%s)
+    time_diff=$(echo "$end_time - $start_time" | bc)
+    printf "\n>> jobs for ${lib_id} completed in %s seconds\n" $time_diff
+
+done < $SAMPLE_LIST
