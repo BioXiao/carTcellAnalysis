e09dd2ab05c899f2377c9a8d3af44e760b501e73
James Eddy <james.a.eddy@gmail.com>
2016-03-29 16:50:31 -0700

    finished script to read, format coverage info from rapmap:

--- compile_rapmap_data.R
+++ compile_rapmap_data.R
@@ -1,20 +1,81 @@
-library(CoverageView)
 library(GenomicRanges)
 library(rtracklayer)
-library(Rsamtools)
 library(GenomicAlignments)
-library(ggbio)
+library(ggplot2)
+library(dplyr)
+# library(ggbio)
 
-bam_file <- "~/Box Sync/data/projects/carTcellAnalysis/results/rapmap/150910_D00565_0096_AC6REVANXX/P89-10/lib9207_C6REVANXX.bam"
-cov_bam_file <- CoverageView::CoverageBamFile(bam_file)
+gff_file <- "~/Box Sync/data/projects/carTcellAnalysis/annotation/carPlus.gtf"
+car_gtf <- import.gff3(gff_file)
 
-temp <- readGAlignments(bam_file)
-cvg <- coverage(temp)
+get_coverage_df <- function(bam_file, seq_name) {
+    lib_id <- str_extract(bam_file, "lib[0-9]+")
+    
+    gal <- readGAlignments(bam_file)
+    cov <- coverage(gal)
+    
+    seq_cov <- cov[[seq_name]]
+    seq_len <- length(seq_cov)
+    cov_df <- data_frame(pos = 1:seq_len,
+                         cov = as.vector(window(seq_cov, 1, seq_len)))
+    return(cov_df)
+}
 
-car_cov <- cvg[["CAR-1"]]
-car_smooth_cov <- round(runmean(car_cov, 101, endrule = "constant"))
-df <- data_frame(cov = as.vector(window(car_smooth_cov, 1, length(car_cov))))
-df %>% ggplot(aes(x = x, y = y)) +
-    geom_line() +
-    geom_smooth(se = FALSE)
+get_coverage <- function(aln, gtf, seq, meta = NULL, smooth = 0, min.coverage = 0) {
 
+    ## set boundaries of region to plot: approx. the range of all transcripts
+    message("Calculating boundaries of region of interest...")
+    zoom <- reduce(gtf, min.gapwidth = 100e4)
+    zoom <- zoom[which(seqnames(zoom) == seq)]
+    if (smooth > 0) {
+        ## calculated smoothed coverage
+        message("Smoothing coverage in windows of size ", smooth, " bp...")
+        bins <- seq(start(zoom), end(zoom), smooth)
+        bins_gr <- GRanges(seqnames = as.vector(seqnames(zoom))[1], 
+                           ranges = IRanges(start = bins [-length(bins)], end = bins[-1]-1))
+
+        cov_df <- transform(as.data.frame(bins_gr), 
+                            score = countOverlaps(bins_gr, aln)) %>% 
+            mutate(midpoint = (start + end) / 2) %>% 
+            select(pos = midpoint, cov = score)
+    }
+    else {
+        ## calculate raw coverage
+        message("Estimating read coverage...")
+        cvg <- coverage(aln)[[seq]]
+        
+        seq_len <- length(cvg)
+        cov_df <- data_frame(pos = 1:seq_len,
+                             cov = as.vector(window(cvg, 1, seq_len)))
+    }
+    
+    return(cov_df)
+}
+
+build_coverage_df <- function(bam_file_list, gtf, seq_name, smooth = 0) {
+    lapply(bam_file_list, function(x) {
+        lib_id <- str_extract(x, "lib[0-9]+")
+        aln <- readGAlignments(x)
+        
+        cov_df <- get_coverage(aln, gtf, seq_name, smooth = smooth) %>% 
+            mutate(lib_id = lib_id)
+    }) %>% 
+        bind_rows()
+}
+
+
+plot_coverage <- function(cov_df, xlims) {
+    cov_df %>% 
+        ggplot(aes(x = pos, y = cov, colour = lib_id)) +
+        geom_point(alpha = 0.5) +
+        geom_smooth(se = FALSE) +
+        scale_color_viridis(discrete = TRUE) +
+        theme_bw()
+}
+
+
+bam_files <- list.files("~/Box Sync/data/projects/carTcellAnalysis/results/rapmap/",
+                        full.names = TRUE, recursive = TRUE) %>% 
+    .[str_detect(., ".bam$")]
+
+cov_df <- build_coverage_df(as.list(bam_files), car_gtf, "CAR-1", 10)
