860db7504eef2b7a9f0e12a56beecf2d06c7df1e
James Eddy <james.a.eddy@gmail.com>
2016-05-04 14:01:40 -0700

    fixed script arguments to reflect updates; added force option

--- prep_ref_data.sh
+++ prep_ref_data.sh
@@ -3,6 +3,17 @@
 # This script is used to prepare reference sequence and annotation data used
 # for
 
+# Specify whether to force generation of formatted reference data, even if
+# files already exist
+FORCE=0
+while getopts "f" ARG; do
+	case "$ARG" in
+	    f ) FORCE=1;;
+	esac
+done
+shift $(($OPTIND - 1))
+
+
 # Specify raw sequence text file input; file should already exist in a folder
 # labeled 'sequence' under the 'data' directory
 SEQUENCE_DIR="data/sequence"
@@ -10,10 +21,10 @@ RAW_CAR_SEQ="${SEQUENCE_DIR}/car_transcript_raw.txt"
 
 # Specify human genome FASTA and gene model GTF input; file should exist in a
 # folder labeled 'annotation' under the 'data' directory
-HG38_GENOME_FASTA="${SEQUENCE_DIR}/hg38_genome.fa"
+HG38_GENOME_FASTA="${SEQUENCE_DIR}/hg38/hg38_genome.fa"
 
 ANNOTATION_DIR="data/annotation"
-HG38_XCRIPT_GTF="${ANNOTATION_DIR}/hg38_genes.gtf"
+HG38_XCRIPT_GTF="${ANNOTATION_DIR}/hg38/hg38_genes.gtf"
 
 ### FORMAT REFERENCE SEQUENCE
 
@@ -32,7 +43,7 @@ CAR_XCRIPT_FASTA="${SEQUENCE_DIR}/car_transcript.fa"
 MERGE="True"
 TRANSCRIPT="True"
 
-if [ ! -e "$GENE_XCRIPT_FASTA" ]; then
+if [ ! -e "$GENE_XCRIPT_FASTA" ] || [ ${FORCE} == 1 ]; then
     python scripts/format_fasta.py \
         $RAW_CAR_SEQ \
         $NAME \
@@ -48,9 +59,9 @@ fi
 # HG38_GENOME_FASTA: reference human genome (hg38) from iGenomes
 # HG38_XCRIPT_GTF: reference gene models for hg38 from iGenomes
 
-HG38_XCRIPT_FASTA="${ANNOTATION_DIR}/hg38_transcripts.fa"
+HG38_XCRIPT_FASTA="${ANNOTATION_DIR}/hg38/hg38_transcripts.fa"
 
-if [ ! -e "$HG38_XCRIPT_FASTA" ]; then
+if [ ! -e "$HG38_XCRIPT_FASTA" ] || [ ${FORCE} == 1 ]; then
     gffread \
         -w $HG38_XCRIPT_FASTA
         -g $HG38_GENOME_FASTA
@@ -66,9 +77,9 @@ if [ ! -e "$HG38_XCRIPT_FASTA" ]; then
 # HG38_XCRIPT_FASTA: hg38 transcriptome FASTA file
 # CUSTOM_XCRIPTOME_FASTA: concatenated transcriptome FASTA
 
-CUSTOM_XCRIPTOME_FASTA="${SEQUENCE_DIR}/hg38_car_transcripts.fa"
+CUSTOM_XCRIPTOME_FASTA="${SEQUENCE_DIR}/hg38/hg38_car_transcripts.fa"
 
-if [ ! -e "$CUSTOM_XCRIPTOME_FASTA" ]; then
+if [ ! -e "$CUSTOM_XCRIPTOME_FASTA" ] || [ ${FORCE} == 1 ]; then
     cat $CAR_XCRIPT_FASTA \
         $HG38_XCRIPT_FASTA \
         > $CUSTOM_XCRIPTOME_FASTA
@@ -92,7 +103,7 @@ CAR_PARTS_FASTA="${SEQUENCE_DIR}/car_parts.fa"
 MERGE="False"
 TRANSCRIPT="False"
 
-if [ ! -e "$CAR_PARTS_FASTA" ]; then
+if [ ! -e "$CAR_PARTS_FASTA" ]; || [ ${FORCE} == 1 ] then
     python scripts/format_fasta.py \
         $RAW_CAR_SEQ \
         $NAME \
@@ -103,21 +114,23 @@ fi
 
 # From CAR parts FASTA, create gene model GTF; transcript ID is used as
 # chromosome name; each segment is labeled as a unique transcript (note:
-# segments are trimmed by 2bp from each direction such that the start/end
+# segments are trimmed by 1bp from each direction such that the start/end
 # is easier to see with track visualization like IGV)
 
 # CAR_PARTS_FASTA: FASTA file with a separate record for each segment of the
 #   CAR construct
-# NAME: name of gene/transcript/construct (used for labelling)
 # CAR_PARTS_GTF: name of output gene model GTF file
+# SPACER: number of bp to trim from both ends of each part to create a visual
+#   gap when viewing in browser
 
 CAR_PARTS_GTF="${ANNOTATION_DIR}/car_parts.gtf"
+SPACER=1
 
-if [ ! -e "$CAR_PARTS_GTF" ]; then
-    python scripts/gene_fasta_to_gtf.py \
+if [ ! -e "$CAR_PARTS_GTF" ] || [ ${FORCE} == 1 ]; then
+    python scripts/parts_fasta_to_gtf.py \
         $CAR_PARTS_FASTA \
-        $NAME \
-        $CAR_PARTS_GTF
+        $CAR_PARTS_GTF \
+        $SPACER
 fi
 
 # Create pseudo-GTF records for human transcripts that overlap with CAR
@@ -126,61 +139,27 @@ fi
 
 # CAR_PARTS_OVERLAP_GTF: gene model records for transcripts that overlap with
 #   CAR construct sequence
+# CAR_PARTS_OVERLAP_GTF: name of output gene model GTF file
+# SPACER: number of bp to trim from both ends of each part to create a visual
+#   gap when viewing in browser
 
 CAR_PARTS_OVERLAP_GTF="${ANNOTATION_DIR}/car_parts_overlap.gtf"
+SPACER=0
 
-printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" \
-    'NM_000758' 'custom' 'transcript' '1' '784' '.' '+' '.' \
-    'gene_id "CSF2"; transcript_id "GMCSFRss_r1";' \
-    > $CAR_PARTS_OVERLAP_GTF
-printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" \
-    'NM_001243077' 'custom' 'transcript' '1' '4588' '.' '+' '.' \
-    'gene_id "CD28"; transcript_id "CD28tm_r1";' \
-    >> $CAR_PARTS_OVERLAP_GTF
-printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" \
-    'NM_001243078' 'custom' 'transcript' '1' '4522' '.' '+' '.' \
-    'gene_id "CD28"; transcript_id "CD28tm_r2";' \
-    >> $CAR_PARTS_OVERLAP_GTF
-printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" \
-    'NM_006139' 'custom' 'transcript' '1' '4878' '.' '+' '.' \
-    'gene_id "CD28"; transcript_id "CD28tm_r3";' \
-    >> $CAR_PARTS_OVERLAP_GTF
-printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" \
-    'NM_001561' 'custom' 'transcript' '1' '5984' '.' '+' '.' \
-    'gene_id "TNFRSF9"; transcript_id "IgG4hinge_r1";' \
-    >> $CAR_PARTS_OVERLAP_GTF
-printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" \
-    'NM_000734' 'custom' 'transcript' '1' '1676' '.' '+' '.' \
-    'gene_id "CD247"; transcript_id "CD3Zeta_r1";' \
-    >> $CAR_PARTS_OVERLAP_GTF
-printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" \
-    'NM_198053' 'custom' 'transcript' '1' '1679' '.' '+' '.' \
-    'gene_id "CD247"; transcript_id "CD3Zeta_r2";' \
-    >> $CAR_PARTS_OVERLAP_GTF
-printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" \
-    'NM_201283' 'custom' 'transcript' '1' '1570' '.' '+' '.' \
-    'gene_id "EGFR"; transcript_id "EGFRt_r1";' \
-    >> $CAR_PARTS_OVERLAP_GTF
-printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" \
-    'NM_201282' 'custom' 'transcript' '1' '2236' '.' '+' '.' \
-    'gene_id "EGFR"; transcript_id "EGFRt_r2";' \
-    >> $CAR_PARTS_OVERLAP_GTF
-printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" \
-    'NM_201284' 'custom' 'transcript' '1' '2861' '.' '+' '.' \
-    'gene_id "EGFR"; transcript_id "EGFRt_r3";' \
-    >> $CAR_PARTS_OVERLAP_GTF
-printf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n" \
-    'NM_005228' 'custom' 'transcript' '1' '5592' '.' '+' '.' \
-    'gene_id "EGFR"; transcript_id "EGFRt_r4";' \
-    >> $CAR_PARTS_OVERLAP_GTF
+if [ ! -e "$CAR_PARTS_OVERLAP_GTF" ] || [ ${FORCE} == 1 ]; then
+    python scripts/parts_fasta_to_gtf.py \
+        $CAR_PARTS_OVERLAP_FASTA \
+        $CAR_PARTS_OVERLAP_GTF \
+        $SPACER
+fi
 
 # Finally, create combined GTF file
 
-CAR_PLUS_REF_GTF="${ANNOTATION_DIR}/car_plus_ref.gtf"
+CAR_PLUS_OVERLAP_GTF="${ANNOTATION_DIR}/car_plus_overlap.gtf"
 
-if [ ! -e "$CAR_PLUS_REF_GTF"]; then
+if [ ! -e "$CAR_PLUS_REF_GTF" || [ ${FORCE} == 1 ]]; then
     cat \
         $CAR_PARTS_GTF \
         $CAR_PARTS_OVERLAP_GTF \
-        > $CAR_PLUS_REF_GTF
+        > $CAR_PLUS_OVERLAP_GTF
 fi
