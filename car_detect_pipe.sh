915f8f01c343361b0d7590ef9b662b172ec5d9e9
James Eddy <james.a.eddy@gmail.com>
2016-05-04 17:07:34 -0700

    lots of updates, including new tool versions, speed-ups, and status messages

--- car_detect_pipe.sh
+++ car_detect_pipe.sh
@@ -8,26 +8,28 @@ RAPMAP_INDEX_DIR="data/indexes/rapmap"
 SALMON_INDEX_DIR="data/indexes/salmon"
 
 # Define executable paths
-RAPMAP_EXEC="docker run --rm -v ${PWD}/data:/home/data jaeddy/rapmap:0.1.0-pre rapmap"
+RAPMAP_IMG="docker run --rm -v ${PWD}/data:/home/data jaeddy/rapmap:0.2.1"
 SAMTOOLS_EXEC="samtools"
 SALMON_EXEC="tools/SalmonBeta-0.5.0_OSX-10.10/bin/salmon"
 
 # Build indexes for the modified reference transcriptome, which includes the
 # gene construct sequence as an additional transcript
-HG38_CAR_FASTA="${SEQUENCE_DIR}/hg38_CAR_transcripts.fa"
+HG38_CAR_FASTA="${SEQUENCE_DIR}/hg38/hg38_car_transcripts.fa"
 
 # Build RapMap quasiindex for hg38 + CAR transcript
-HG38_CAR_RAPMAP_INDEX="${RAPMAP_INDEX_DIR}/hg38_CAR"
+HG38_CAR_RAPMAP_INDEX="${RAPMAP_INDEX_DIR}/hg38_car"
 if [ ! -e "$HG38_CAR_RAPMAP_INDEX" ]; then
     echo "Building hg38 + CAR RapMap quasiindex..."
-    $RAPMAP_EXEC quasiindex \
+    $RAPMAP_IMG rapmap quasiindex \
         -t $HG38_CAR_FASTA \
         -i $HG38_CAR_RAPMAP_INDEX \
         -k 19
+else
+    echo "hg38 + CAR RapMap quasiindex already exists; skipping."
 fi
 
 # Build RapMap quasiindex for hg38 + CAR transcript
-HG38_CAR_SALMON_INDEX="${SALMON_INDEX_DIR}/hg38_CAR"
+HG38_CAR_SALMON_INDEX="${SALMON_INDEX_DIR}/hg38_car"
 if [ ! -e "$HG38_CAR_SALMON_INDEX" ]; then
     echo "Building hg38 + CAR Salmon quasiindex..."
     $SALMON_EXEC index \
@@ -35,12 +37,14 @@ if [ ! -e "$HG38_CAR_SALMON_INDEX" ]; then
         -i $HG38_CAR_SALMON_INDEX \
         --type quasi \
         -k 19
+else
+    echo "hg38 + CAR Salmon quasiindex already exists; skipping."
 fi
 
 RAPMAP_OUT_DIR="data/results/rapmap"
 SALMON_OUT_DIR="data/results/salmon"
 
-XCRIPT_LIST=$(grep ">" ${SEQUENCE_DIR}/combined_CAR_endo.fa \
+XCRIPT_LIST=$(grep ">" ${SEQUENCE_DIR}/car_parts_overlap.fa \
               | awk -F" " '{gsub(">", ""); print $1}')
 
 # Run RapMap and Salmon on all samples
@@ -49,7 +53,7 @@ while read line; do
     sleep 1
 
     file=$(echo $line | awk '{print $1}')
-    lib_id=$(echo $file | egrep -o -e "lib[0-9]+.*XX")
+    lib_id=$(echo $file | egrep -o -e "lib[0-9]+")
     fc=$(echo $line | awk '{print $2}')
     proj=$(echo $line | awk '{print $3}')
 
@@ -67,26 +71,33 @@ while read line; do
     if [ ! -e "$tmp_dir" ]; then
         mkdir "$tmp_dir"
     fi
-    tmp_prefix="${tmp_dir}/${lib_id}"
+    tmp_prefix="${lib_id}"
     out_prefix="${RAPMAP_OUT_DIR}/${out_subdir}/${lib_id}"
     printf "\n>> Quasimap %s to %s...\n" "${lib_id}" "${HG38_CAR_RAPMAP_INDEX}"
     if [ ! -e "${out_prefix}.bam" ]; then
+        printf "\n...getting input FASTQ...\n"
         if [ ! -e "${tmp_dir}/$(basename $file)" ]; then
             time rsync -h --progress $file "${tmp_dir}/$(basename $file)"
         fi
-        time $RAPMAP_EXEC quasimap -t 4 \
-            -i $HG38_CAR_RAPMAP_INDEX \
-            -r "${tmp_dir}/$(basename $file)" \
-            -o ${tmp_prefix}.sam
-
-        echo "Formatting output..."
-        time ($SAMTOOLS_EXEC view -bS -@ 4 ${tmp_prefix}.sam \
-            > ${tmp_prefix}.bam && \
-            $SAMTOOLS_EXEC sort -@ 4 ${tmp_prefix}.bam ${tmp_prefix}_sorted && \
-            $SAMTOOLS_EXEC index ${tmp_prefix}_sorted.bam && \
-            $SAMTOOLS_EXEC view -b -@ 4 ${tmp_prefix}_sorted.bam $XCRIPT_LIST \
-            > ${out_prefix}.bam && \
-            $SAMTOOLS_EXEC index ${out_prefix}.bam)
+#         read -r -d '' rapmap_cmd << EOF
+# rapmap && \
+#     samtools
+# EOF
+        read -r -d '' rapmap_cmd << EOF
+printf "\n...running RapMap...\n" && \
+    time rapmap quasimap -t 4 \
+    -i $HG38_CAR_RAPMAP_INDEX \
+    -r "${tmp_dir}/$(basename $file)" \
+    | samtools view -bS -@ 4 - > ${tmp_prefix}.bam && \
+    printf "\n...sorting BAM output...\n" && \
+    time samtools sort -@ 4 -m 2G -o ${tmp_prefix}_sorted.bam ${tmp_prefix}.bam && \
+    samtools index ${tmp_prefix}_sorted.bam && \
+    printf "\n...filtering sorted BAM...\n" && \
+    time samtools view -b -@ 4 ${tmp_prefix}_sorted.bam $XCRIPT_LIST \
+    > ${out_prefix}.bam && \
+    samtools index ${out_prefix}.bam
+EOF
+        $RAPMAP_IMG /bin/bash -c "$(echo $rapmap_cmd)"
     else
         echo "Output already exists."
     fi
@@ -96,6 +107,7 @@ while read line; do
     out_prefix="${SALMON_OUT_DIR}/${out_subdir}/${lib_id}"
     printf "\n>> Quant %s with %s...\n" "${lib_id}" "${HG38_CAR_SALMON_INDEX}"
     if [ ! -e "${out_prefix}/quant.sf" ]; then
+        printf "\n...running Salmon...\n"
         time $SALMON_EXEC quant \
             -i $HG38_CAR_SALMON_INDEX \
             -l U \
